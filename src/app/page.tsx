c7e8daa33981e177bd9cf87e60043d8fe09521ee
Bruno_Neves <nevesbruno814@gmail.com>
2025-05-17 23:54:02 -0400

    adicionando thema dark e light

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeToggle } from "@/components/theme/theme-toggle"
 import { Button } from "@/components/ui/button"
 
 const Page = () => {
     return (
         <div>
             <Button>Button</Button>
+
+            <ThemeToggle />
         </div>
     )
 }
